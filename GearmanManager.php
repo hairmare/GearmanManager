9e7335ae97c7d6daa61cceb3b96aa7a631f836c4
Lucas Bickel <hairmare@purplehaze.ch>
2012-04-29 14:36:19 +0200

    bugfix

--- GearmanManager.php
+++ GearmanManager.php
@@ -351,8 +351,8 @@ abstract class GearmanManager {
             if($opts["l"] === 'syslog'){
                 $this->log_syslog = true;
             } else {
-                $this->log_file = $opts["l"];
-                $this->open_log_file($this->log_file);
+                $this->config['log_file'] = $opts["l"];
+                $this->open_log_file($this->config['log_file']);
             }
         }
 
